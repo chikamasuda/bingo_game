2ebbad7930fd60576c187104a09a0f0bd9cf6f41
chikamasuda <c.m.062820@gmail.com>
2021-07-13 08:24:18 +0900

    ビンゴシート大体完成

--- js/main.js
+++ js/main.js
@@ -7,7 +7,6 @@
     }
 
     const column = [];
-
     for (let i = 0; i < 5; i++) {
       column[i] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
     }
@@ -22,8 +21,6 @@
   columns[4] = createColumn(4);
   columns[2][2] = 'FREE';
 
-  console.table(columns);
-
   const bingo = [];
   for (let row = 0; row < 5; row++) {
     bingo[row] = [];
@@ -32,4 +29,14 @@
     }
   }
   console.table(bingo);
+
+  for (let row = 0; row < 5; row++) {
+    const tr = document.createElement('tr');
+    for (let col = 0; col < 5; col++) {
+      const td = document.createElement('td');
+      td.textContent = bingo[row][col];
+      tr.appendChild(td);
+    }
+    document.querySelector('tbody').appendChild(tr);
+  }
 }
